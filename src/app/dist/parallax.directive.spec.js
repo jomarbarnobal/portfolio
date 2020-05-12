3c44c3b37457c708f923294ae80777af17df9f96
dev <dev@kali.dev>
2020-05-11 20:30:25 -0700

    fixed sass import

--- src/app/dist/parallax.directive.spec.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var parallax_directive_1 = require("./parallax.directive");
-describe('ParallaxDirective', function () {
-    it('should create an instance', function () {
-        var directive = new parallax_directive_1.ParallaxDirective();
-        expect(directive).toBeTruthy();
-    });
-});
--- src/app/pages/dist/index.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var home_component_1 = require("./home/home.component");
-exports.Home = home_component_1.Home;
